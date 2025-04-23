13fb7799eca666d247e33633d05e61d4150024c9
bhavana <bhavanabc017@gmail.com>
2025-04-23 22:44:32 +0530

    x

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,10 @@ const port=process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://e-com-fooddel.onrender.com",
+  credentials: true
+}));
 
 
 //api endpoints 
