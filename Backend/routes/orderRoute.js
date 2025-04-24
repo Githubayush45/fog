bf20c67e80e82a11f62015cd789d05b13a514480
bhavana <bhavanabc017@gmail.com>
2025-04-24 23:01:47 +0530

    changes

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -3,7 +3,10 @@ import authMiddleware from '../middleware/auth.js';
 import { placeOrder, getOrdersWithUserInfo, updateOrderStatus, getUserOrders } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
-
+orderRouter.get('/', (req, res) => {
+    res.send("Orders API is working");
+  });
+  
 orderRouter.post('/place', authMiddleware, placeOrder);
 orderRouter.get('/orders', getOrdersWithUserInfo);
 orderRouter.get('/user/orders', authMiddleware, getUserOrders);
